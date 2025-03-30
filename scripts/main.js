3c1e62f5e67c710c9415661cf2466ea06d8413fb
Neon <78184988+NEON-XZR@users.noreply.github.com>
2025-03-31 03:17:45 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,12 @@ mod.meta.displayName = Core.bundle.get("mod.name");
 mod.meta.description = Core.bundle.get("mod.description");
 // special thanks to meep for the help!!
 
+
+//music import
 let boss2v2 = Vars.tree.loadMusic("boss2v2");
+let despair = Vars.tree.loadMusic("despair");
 
 Events.on(ClientLoadEvent, e => {
   Vars.control.sound.darkMusic.add(boss2v2);
+  Vars.control.sound.ambientMusic.add(despair);
 });
