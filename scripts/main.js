2692d05ebdeea88408a718ac0a63e9658e8e535f
Neon <78184988+NEON-XZR@users.noreply.github.com>
2025-05-10 09:31:24 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,12 @@ mod.meta.description = Core.bundle.get("mod.description");
 //music import
 let boss2v2 = Vars.tree.loadMusic("boss2v2");
 let despair = Vars.tree.loadMusic("despair");
+let demh = Vars.tree.loadMusic ("demh");
+let fam = Vars.tree.loadMusic ("fam");
 
 Events.on(ClientLoadEvent, e => {
   Vars.control.sound.darkMusic.add(boss2v2);
   Vars.control.sound.ambientMusic.add(despair);
+  Vars.control.sound.ambientMusic.add(demh);
+  Vars.control.sound.ambientMusic.add(fam);
 });
